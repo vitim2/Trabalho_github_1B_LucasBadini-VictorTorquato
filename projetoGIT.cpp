7f74cb43fe443feb7dc2f8efec4bad33f5e2ce72
Lukin <55561423+Lukin-github@users.noreply.github.com>
2019-11-20 16:02:46 -0300

    Adicionei a função main.

--- projetoGIT.cpp
+++ projetoGIT.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
 int contaLetras(char letra, string texto,int qtdletras)
 {
   int i,qtd=0;
@@ -35,4 +39,35 @@ void substituirPalavras(string palavra1,string palavra2,string texto)
 		pst=texto.find(palavra1,pst+1);
 	}
 	return texto;
+}
+
+int main()
+{
+  string texto, palavra1, palavra2;
+  char letra, substituta;
+  int len;
+
+  cout<<"Insira o texto base: \n\n";
+  get.line(cin,texto);
+
+  len=texto.length();
+
+  cout<<"\n\nInsira o caractere que deseja buscar: ";
+  cin>>letra;
+  cout<<"\nA letra"<<letra<<" aparece "<< contaLetras(letra,texto,len) <<"vezes.\n";
+
+  cout<<"\nDigite o caractere a ser inserido no lugar: ";
+  cin>>substituta;
+  substituirLetra(letra,substituta,texto,len);
+  cout<<"\n\nNova versao: \n\n"<<texto;
+
+  cout<<"\n\nIntroduza a palavra a ser buscada: ";
+  get.line(cin,palavra1);
+  cout<<"\n\nA palavra "<<palavra1<<" aparece "<<contaPalavras(palavra1,texto)<<" vezes.";
+
+  cout<<"\n\nDigite a palavra a ser inserida no lugar: ";
+  get.line(cin,palavra2);
+  substituirPalavras(palavra1,palavra2,texto);
+  cout<<"\n\nNova versao:\n\n"<<texto;
+  return 0;
 }
