42d3804bdcde87e15e3db19c483ac376bf34694a
vitim2 <57270753+vitim2@users.noreply.github.com>
2019-11-20 15:54:29 -0300

    Update projetoGIT.cpp

--- projetoGIT.cpp
+++ projetoGIT.cpp
@@ -6,3 +6,33 @@ int contaLetras(char letra, string texto,int qtdletras)
       qtd++;
   return qtd;
 }
+void substituirLetra(char letra1,char letra2, string texto,int qtdletras)
+{
+  int i;
+  for (i=0;i<qtdletras;i++)
+    if(texto[i]==letra1)
+      texto[i]=letra2;
+}
+
+int contaPalavras(string palavra, string texto)
+{
+	int qtd=0,pst;
+	pst=texto.find(palavra,0);
+	while(pst!=-1)
+	{
+		qtd++;
+		pst=texto.find(palavra,pst+1);
+	}
+	return qtd;
+}
+void substituirPalavras(string palavra1,string palavra2,string texto)
+{
+	int i,pst;
+	pst=texto.find(palavra1,0);
+	while(pst!=-1)
+	{
+		texto.replace(pst,palavra1.length(),palavra2);
+		pst=texto.find(palavra1,pst+1);
+	}
+	return texto;
+}
